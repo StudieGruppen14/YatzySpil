ed0aeb27d0f3b55e29396e2508bdac83b1d2c71d
mett714a <44974139+mett714a@users.noreply.github.com>
2018-11-12 18:05:54 +0100

    Add files via upload

---
+++ Yatzy_Ny/Yatzy_Ny/Bæger.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace Yatzy_Ny
+{
+    public class Bæger
+    {
+		public Terning Terning1 = new Terning();
+        public Terning Terning2 = new Terning();
+        public Terning Terning3 = new Terning();
+        public Terning Terning4 = new Terning();
+        public Terning Terning5 = new Terning();
+
+		public void RystBæger()
+        {
+			Terning1.KastTerning();
+            Terning2.KastTerning();
+            Terning3.KastTerning();
+            Terning4.KastTerning();
+            Terning5.KastTerning();
+		}
+
+		private int HvorMangeAfEnSlags(int Slags)
+        {
+			int Antal = 0; 
+
+			if (Terning1.Øjne == Slags)
+				( Antal = Antal = 1; ) 
+            if (Terning2.Øjne == Slags)
+                ( Antal = Antal = 1; )
+            if (Terning3.Øjne == Slags)
+                ( Antal = Antal = 1; )
+            if (Terning4.Øjne == Slags)
+                ( Antal = Antal = 1; )
+            if (Terning5.Øjne == Slags)
+                ( Antal = Antal = 1; ) 
+           return Antal; 
+		}
+        
+		public int Point1ere()
+        {
+			return HvorMangeAfEnSlags(1);
+		}
+      
+        public int Point2ere()
+        {
+            return HvorMangeAfEnSlags(2); = 2 // ???
+        }
+
+        public int Point3ere()
+        {
+            return HvorMangeAfEnSlags(3); = 3 // ???
+        }
+
+        public int Point4ere()
+        {
+            return HvorMangeAfEnSlags(4); = 4 // ???
+        }
+
+        public int Point5ere()
+        {
+            return HvorMangeAfEnSlags(5); = 5 // ???
+        }
+
+        public int Point6ere()
+        {
+            return HvorMangeAfEnSlags(6); = 6 // ???
+        }
+
+        // Point muligheder 
+
+
+	}
---
+++ Yatzy_Ny/Yatzy_Ny/Terning.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Yatzy_Ny
+{
+    internal class Terning
+    {
+        private int AntalØjne;
+        private static Random r = new Random();
+        private Boolean HoldMig = false;
+
+        public int Øjne
+        {
+            get ( return AntalØjne; )
+
+        }
+
+    public Boolean Holdt
+    {
+        get ( return HoldMig; )
+        }
+
+        public void KastTerning()
+    {
+        if (HoldMig == false)
+        { AntalØjne = r.Next(1, 7); }
+    }
+
+    public void HoldTerning()
+    {
+        HoldMig = true;
+    }
+
+    public void TagTerningMed()
+    {
+        HoldMig = false;
+    }
+}
+}
